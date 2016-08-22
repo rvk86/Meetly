dd15288866c4e0f09b55a1c6c4a87b8db1e1ce39
Ralph van Krimpen <ralph.vankrimpen@gmail.com>
2016-08-21 21:03:50 -0500

    Working on gapi service

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,9 +1,7 @@
 import angular from 'angular';
-import Navbar from './navbar/navbar';
 import Gapi from './gapi/gapi';
 
 let commonModule = angular.module('app.common', [
-  Navbar,
   Gapi
 ])
 
--- client/app/common/gapi/gapi.service.js
+++ client/app/common/gapi/gapi.service.js
@@ -1,42 +1,71 @@
+import moment from 'moment';
+import './client.js';
+
 class GapiService {
 
   constructor($location, $rootScope, config) {
     'ngInject';
 
+    this.script_id = config.gapi_script_id;
     this.client_id = config.gapi_client_id;
     this.scopes = config.gapi_scopes;
+    this.calendar_ids = config.calendar_ids;
     this.rootScope = $rootScope;
     this.location = $location;
 
   }
 
-  auth(immediate, location) {
+  auth(immediate, location='') {
 
-    gapi.auth.authorize(
-      {
-        'client_id': this.client_id,
-        'scope': this.scopes.join(' '),
-        'immediate': immediate
-      }, (authResult) => {
+    gapi.auth.authorize({
+      'client_id': this.client_id,
+      'scope': this.scopes.join(' '),
+      'immediate': immediate
+    }, (authResult) => {
 
-        if (authResult && !authResult.error) {
-          this.rootScope.$apply(() => {
+      if (authResult && !authResult.error) {
 
-            this.location.path(location);
+          this.run('getUser').then((result) => {
+            this.rootScope.$apply(() => {
+              this.user = result.result;
+              this.initialized = true;
+              this.location.path(location);
+            });
+          });
 
-          })
-        } else {
-          throw 'We are sorry, the Google authentication failed!';
-        }
+      } else {
 
-      });
+        throw 'We are sorry, the Google authentication failed!';
+
+      }
+
+    });
 
   }
 
 
-  loadCalendarApi() {
+  run(method, args=[]) {
 
-    gapi.client.load('calendar', 'v3');
+    var request = {
+      'function': method,
+      'parameters': args,
+      // 'devMode': true
+    };
+
+    return gapi.client.request({
+      'root': 'https://script.googleapis.com',
+      'path': 'v1/scripts/' + this.script_id + ':run',
+      'method': 'POST',
+      'body': request
+    }).then((result) => {
+
+      return result.result.response;
+
+    }, (error) => {
+
+      return error;
+
+    });
 
   }
 
